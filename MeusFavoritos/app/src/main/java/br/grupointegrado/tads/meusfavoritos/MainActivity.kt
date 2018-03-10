01f8c2a0b5a7f9797c9217ab78ad1ba19f808dd6
heyWillz <willz3@outlook.com>
2018-03-09 21:09:08 -0300

    Scrollview com List

--- MeusFavoritos/app/src/main/java/br/grupointegrado/tads/meusfavoritos/MainActivity.kt
+++ MeusFavoritos/app/src/main/java/br/grupointegrado/tads/meusfavoritos/MainActivity.kt
@@ -2,11 +2,21 @@ package br.grupointegrado.tads.meusfavoritos
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        preencherLista()
+    }
+
+    fun preencherLista(){
+        for (fav in Favoritos.getLista()){
+            textu.append("$fav \n\n\n")
+        }
+
     }
 }
